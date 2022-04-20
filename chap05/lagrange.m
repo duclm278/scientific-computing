48d955f42fe23c03e2c5c117b26429ad03e299bc
lmd278 <palered548@gmail.com>
2022-04-20 19:27:02 +0700

    Minor changes

--- chap05/lagrange.m
+++ chap05/lagrange.m
@@ -2,13 +2,13 @@
 
 function [result] = lagrange(fx, a, b, m)
 
-xi = @(i) a + (i - 1)*(b - a)/m;
-xj = @(j) a + (j - 1)*(b - a)/m;
+xi = @(i) a + i*(b - a)/m;
+xj = @(j) a + j*(b - a)/m;
 
 result = 0;
-for i = 1:(m + 1)
+for i = 0:m
     product = @(x) 1;
-    for j = 1:(m + 1)
+    for j = 0:m
         if i ~= j
             product = @(x) product(x).*(x - xj(j))/(xi(i) - xj(j));
         end
